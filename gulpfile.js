73c8c33b54681a3d22958016224cc4362c9e64e9
art <art@cowell.com.tw>
2017-06-28 11:26:14 +0800

    add gulp for test report

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp')
+var chai = require('chai')
+var mocha = require('gulp-mocha')
+
+gulp.task('default', ['test'], function () {
+  console.log('My Default Task')
+})
+
+gulp.task('test',['testlab01','testlab02'],function(){})
+
+gulp.task('testlab01',function () {
+  console.log('Run Unit Test LAB01 With Mocha')
+  return gulp.src(['lab01/test/test.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'report',
+        reportFilename: 'lab01',
+        reportTitle: 'Test01Report'
+        // inlineAssets: true
+      },
+      globals: {
+        should: require('chai')
+      }
+    }))
+})
+
+gulp.task('testlab02', function () {
+  console.log('Run Unit Test LAB02 With Mocha')
+  return gulp.src(['lab02/test.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'report',
+        reportFilename: 'lab02',
+        reportTitle: 'Test02Report'
+        // inlineAssets: true
+      },
+      globals: {
+        should: require('chai')
+      }
+    }))
+})
+
+
+gulp.task('testall', function () {
+  console.log('Run All Unit Test With Mocha')
+  return gulp.src(['lab01/test/test.js','lab02/test.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'report',
+        reportFilename: 'all',
+        reportTitle: 'All Unit Test Report'
+        // inlineAssets: true
+      },
+      globals: {
+        should: require('chai')
+      }
+    }))
+})
+
+
+gulp.task('testlab03', function () {
+  console.log('Run Unit Test LAB03 With Mocha')
+  return gulp.src(['lab03/*.test.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'report',
+        reportFilename: 'lab03',
+        reportTitle: 'Test03Report'
+        // inlineAssets: true
+      },
+      globals: {
+        should: require('chai')
+      }
+    }))
+})
