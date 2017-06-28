46adb38320ef61779faa51931b6cf470edcfc1b9
art <art@cowell.com.tw>
2017-06-28 12:04:48 +0800

    調整Lab01，使其支援node及Browser test

--- browser/tests.js
+++ browser/tests.js
@@ -1,12 +1,20 @@
-var expect = chai.expect;
 
-describe('加法函数的测试', function() {
+try {
+  // node
+	var expect = require("chai").expect;
+  var Calculator = require("../browser/Calculator");
+}catch(e){
+  // browser
+  var expect = chai.expect;
+}
+
+describe('Calculator', function() {
   it('1 加 1 应该等于 2', function() {
-    expect(add(1, 1)).to.be.equal(2);
+    expect(Calculator.add(1, 1)).to.be.equal(2);
   });
 
   it('任何数加0等于自身', function() {
-    expect(add(1, 0)).to.be.equal(1);
-    expect(add(0, 0)).to.be.equal(0);
+    expect(Calculator.add(1, 0)).to.be.equal(1);
+    expect(Calculator.add(0, 0)).to.be.equal(0);
   });
 });
